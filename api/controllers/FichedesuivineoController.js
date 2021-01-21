0171c3e371322d1812ab69a4ae77c3ce0ef0f739
MIHAINGOHERILANTO <ntsoa.s118@gmail.com>
2021-01-21 12:30:22 +0100

    Tableau en details

--- api/controllers/FichedesuivineoController.js
+++ api/controllers/FichedesuivineoController.js
@@ -59,7 +59,7 @@ module.exports = {
         });
     },
 
-    suivideconformite : function(req, res){
+    suivi_conformite : function(req, res){
         if (!req.session.user) return res.redirect('/login');
         var menu = [];
         menu["aceuil"]= "";
@@ -69,10 +69,10 @@ module.exports = {
         menu["presence"]= "";
         menu["admin"]= "";
 
-        var id = parseInt(req.param("id"), 10);
+        var id = req.param("id");
         var my = req.param("my");
-        var annee = parseInt( my.substr( 0, 4), 10);
-        var mois = parseInt( my.substr( 5, 2), 10);
+        var annee = my.substr( 0, 4);
+        var mois = my.substr( 5, 2);
         var sql = "SELECT * FROM neocles_fiche WHERE id_pers ='"+id+"' AND date_part('month', date) = "+mois+" AND date_part('year', date) = "+annee;
         Neocles_fiche.query(sql, function(err, resultat){
             if(err) return res.send(err);
@@ -84,6 +84,32 @@ module.exports = {
             }
         });
         
-    }
+    },
+
+    suivi_en_details : function(req, res){
+        if (!req.session.user) return res.redirect('/login');
+        var menu = [];
+        menu["aceuil"]= "";
+        menu["dossierAdmin"]= "";
+        menu["gestionDossier"]= "";
+        menu["statOpAdmin"]= "";
+        menu["presence"]= "";
+        menu["admin"]= "";
+
+        var id = req.param("id");
+        var my = req.param("my");
+        var annee = my.substr( 0, 4);
+        var mois = my.substr( 5, 2);
+        var sql = "SELECT * FROM neocles_fiche WHERE id_pers ='"+id+"' AND date_part('month', date) = "+mois+" AND date_part('year', date) = "+annee;
+        Neocles_fiche.query(sql, function(err, resultat){
+            if(err) return res.send(err);
+            if(resultat.rowCount == 1){
+                return  res.view('pages/neocles/fiche_de_suivi/tableau_en_details', {layout : false, menu : menu});
+            }
+            else{
+                return  res.view('pages/neocles/fiche_de_suivi/tableau_en_details', {layout : false, menu : menu});
+            }
+        });
+    },
 };
 
