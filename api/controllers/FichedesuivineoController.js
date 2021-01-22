a5a58efe62885ea57a540085b0feb849ef6cef9a
MIHAINGOHERILANTO <ntsoa.s118@gmail.com>
2021-01-22 08:15:01 +0100

    Tableau dynamique

--- api/controllers/FichedesuivineoController.js
+++ api/controllers/FichedesuivineoController.js
@@ -73,16 +73,35 @@ module.exports = {
         var my = req.param("my");
         var annee = my.substr( 0, 4);
         var mois = my.substr( 5, 2);
-        var sql = "SELECT * FROM neocles_fiche WHERE id_pers ='"+id+"' AND date_part('month', date) = "+mois+" AND date_part('year', date) = "+annee;
-        Neocles_fiche.query(sql, function(err, resultat){
+        var sql = "select id_niveau from neo_pers_niveau where id_pers = '"+id+"' and date = (select MAX(date) from neo_pers_niveau where id_pers = '"+id+"')";
+        Neo_pers_niveau.query(sql, function(err, resultat){
             if(err) return res.send(err);
-            if(resultat.rowCount == 1){
-                return  res.view('pages/neocles/fiche_de_suivi/tableau_conformite', {layout : false, menu : menu});
+            if(resultat.rowCount != 0){               
+                if(resultat.rows[0].id_niveau == 5){
+                    var niveau = 2;
+                    var ticket = 20;
+                }
+                else{
+                    var niveau = 1;
+                    var ticket = 13;
+                }
+                sql = "SELECT * FROM neocles_fiche WHERE id_pers ='"+id+"' AND date_part('month', date) = "+mois+" AND date_part('year', date) = "+annee;
+                Neocles_fiche.query(sql, function(err, resultat){
+                    if(err) return res.send(err);
+                    if(resultat.rowCount == 1){
+                        //Le fiche existe déjà
+                        return  res.view('pages/neocles/fiche_de_suivi/tableau_conformite', {layout : false, menu : menu, ticket : ticket});
+                    }
+                    else{
+                        //Le fiche n'existe pas; nous allons le créer
+                        return  res.view('pages/neocles/fiche_de_suivi/tableau_conformite', {layout : false, menu : menu, ticket : ticket});
+                    }
+                });
             }
             else{
-                return  res.view('pages/neocles/fiche_de_suivi/tableau_conformite', {layout : false, menu : menu});
+                return res.send("Numéro matricule incorrecte");
             }
-        });
+        })
         
     },
 
@@ -97,19 +116,38 @@ module.exports = {
         menu["admin"]= "";
         var id = req.param("id");
         var my = req.param("my");
-
         var annee = my.substr( 0, 4);
         var mois = my.substr( 5, 2);
-        var sql = "SELECT * FROM neocles_fiche WHERE id_pers ='"+id+"' AND date_part('month', date) = "+mois+" AND date_part('year', date) = "+annee;
-        Neocles_fiche.query(sql, function(err, resultat){
+
+        var sql = "select id_niveau from neo_pers_niveau where id_pers = '"+id+"' and date = (select MAX(date) from neo_pers_niveau where id_pers = '"+id+"')";
+        Neo_pers_niveau.query(sql, function(err, resultat){
             if(err) return res.send(err);
-            if(resultat.rowCount == 1){
-                return  res.view('pages/neocles/fiche_de_suivi/tableau_en_details', {layout : false, menu : menu});
+            if(resultat.rowCount != 0){               
+                if(resultat.rows[0].id_niveau == 5){
+                    var niveau = 2;
+                    var ticket = 20;
+                }
+                else{
+                    var niveau = 1;
+                    var ticket = 13;
+                }
+                sql = "SELECT * FROM neocles_fiche WHERE id_pers ='"+id+"' AND date_part('month', date) = "+mois+" AND date_part('year', date) = "+annee;
+                Neocles_fiche.query(sql, function(err, resultat){
+                    if(err) return res.send(err);
+                    if(resultat.rowCount == 1){
+                        //Le fiche existe déjà
+                        return  res.view('pages/neocles/fiche_de_suivi/tableau_en_details', {layout : false, menu : menu, ticket : ticket});
+                    }
+                    else{
+                        //Le fiche n'existe pas; nous allons le créer
+                        return  res.view('pages/neocles/fiche_de_suivi/tableau_en_details', {layout : false, menu : menu, ticket : ticket});
+                    }
+                });
             }
             else{
-                return  res.view('pages/neocles/fiche_de_suivi/tableau_en_details', {layout : false, menu : menu});
+                return res.send("Numéro matricule incorrecte");
             }
-        });
+        })
     },
 };
 
