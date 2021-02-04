d46d602f1005cd310350b926181214e835995868
MIHAINGOHERILANTO <ntsoa.s118@gmail.com>
2021-02-04 09:34:50 +0100

    Version betta

--- api/controllers/CoachingController.js
+++ api/controllers/CoachingController.js
@@ -17,6 +17,7 @@ module.exports = {
         menu["presence"]= "";
         menu["admin"]= "";
         const id = req.session.user;
+        
         var create = false;
         if(req.param("create")){
             create = true;
@@ -50,7 +51,7 @@ module.exports = {
         menu["statOpAdmin"]= "";
         menu["presence"]= "";
         menu["admin"]= "";
-        const id = req.session.user;
+        const id = req.session.user;       
         var id_pers = req.param("id");
         var my = req.param("my");
         var annee = my.substr( 0, 4);
@@ -91,12 +92,58 @@ module.exports = {
             }
             else{
                 var date_cr, mois, valider;
-                console.log(mois_now);
                 mois_now = parseInt( mois_now, 10);
-                console.log(mois_now);
-                mois_now = 3;
-                disabled[mois_now] = "";
-                couleur[mois_now] = "vert";
+                for(var i=12; i>=1; i--){
+                    disabled[i] = "";
+                    couleur[i] = "vert";
+                    if(!resultat.rows[i-1]){
+                        if(i > mois_now){
+                            disabled[i] = "disabled";
+                            couleur[i] = "gris";
+                        }
+                        else if(i < mois_now){
+                            if(resultat.rows[i-2] && resultat.rows[i-2].valider){                                
+                                couleur[i] = "red";
+                            }
+                            else if(resultat.rows[i-2] && !resultat.rows[i-2].valider){
+                                disabled[i] = "disabled";
+                                couleur[i] = "gris";
+                            }
+                        }
+                        else{
+                            if(!resultat.rows[i-2]){
+                                disabled[i] = "disabled";
+                                couleur[i] = "gris";
+                            }
+                            else if(resultat.rows[i-2] && !resultat.rows[i-2].valider){
+                                disabled[i] = "disabled";
+                                couleur[i] = "gris";
+                            }
+                            
+                        }
+                    }
+                    else{
+                        date_cr = resultat.rows[i-1].date_compte_rendu;
+                        mois = new Date(date_cr).toISOString().slice(0,10);
+                        mois = parseInt( mois.substr( 5, 2), 10);
+                        valider = resultat.rows[i-1].valider;
+                        if( valider == false){
+                            couleur[mois] = "jaune"; // Mbola tsy valider fa enregistrer
+                            disabled[mois] = "";
+                            disabled[mois+1] = "disabled";
+                            couleur[mois+1] = "gris";                          
+                            if(mois < mois_now){
+                                couleur[mois] = "red";
+                                couleur[mois_now] = "gris"
+                                disabled[mois_now] = "disabled";
+                            }
+                        }
+                        else{
+                            couleur[mois] = "violet";
+                        }
+                    }
+                }
+                /*
                 for(var i=0; i<resultat.rowCount; i++){
                     date_cr = resultat.rows[i].date_compte_rendu;
                     mois = new Date(date_cr).toISOString().slice(0,10);
@@ -111,6 +158,8 @@ module.exports = {
                         couleur[mois] = "jaune";
                         if(mois < mois_now){
                             couleur[mois] = "orange"; //Efa conserver fa tsy valider
+                            disabled[mois_now] = "disabled";
+                            couleur[mois_now] = "gris";
                         }
                     }
                     else{
@@ -118,8 +167,8 @@ module.exports = {
                         couleur[mois] = "violet"; //Efa valider nef afaka jerena
                     }
                 }
-                console.log(disabled);
-                console.log(couleur);
+                
+                */
                 return res.view('pages/neocles/fiche_de_suivi/mois_menu', {layout : false, menu : menu, disabled, couleur,id_pers, annee});
             }
         })
@@ -135,15 +184,18 @@ module.exports = {
         menu["presence"]= "";
         menu["admin"]= "";
         const id = req.session.user;
+        
         var id_pers= req.param("id_pers"), mois = req.param("m"), annee = req.param("y");
         var sql = "select * from neocles_compte_rendu where id_pers ="+id_pers+" and date_part('year', date_compte_rendu) = "+annee+" and date_part('month', date_compte_rendu) = "+mois;
         Neocles_manager.query(sql, function (err, resultat) {
             if(err) return res.send(err);
+            var existe = false;
             if(resultat.rowCount == 1){
-
+                existe = true;
+                return res.view('pages/neocles/fiche_de_suivi/Tableau_cr', {layout : false, menu : menu, mois,id_pers, annee, existe, resultat});
             }
             else{
-                return res.view('pages/neocles/fiche_de_suivi/Tableau_cr', {layout : false, menu : menu, mois,id_pers, annee})
+                return res.view('pages/neocles/fiche_de_suivi/Tableau_cr', {layout : false, menu : menu, mois,id_pers, annee, existe});
             }
         })
     },
@@ -158,22 +210,28 @@ module.exports = {
         menu["presence"]= "";
         menu["admin"]= "";
         const id = req.session.user;
+        
         var date_now = new Date().toISOString().slice(0,10);
         
         var id_pers= req.param("id_pers"), mois = req.param("mois"), annee = req.param("annee");
         var q1 = req.param("q1"), q2 = req.param("q2"), t1 = req.param("t1"), t2 = req.param("t2"), imp1 = req.param("imp1"), imp2 = req.param("imp2");
         var submit = req.param("submit");
-        var sauve = true;
+        var sauve = true, update = false;
         var date_compte_rendu = new Date(annee, mois, 01).toISOString().slice(0,10);
         if(submit == "Conserver"){
             sauve = false;
         }
         var sql = "insert into neocles_compte_rendu(id_pers, id_manager, date_creation, date_compte_rendu, qual_positif, technicite_positif, implication_positif, qual_ameliorer, technicite_ameliorer, implication_ameliorer, valider) values("+id_pers+","+id+",'"+date_now+"','"+date_compte_rendu+"','"+q1+"','"+t1+"','"+imp1+"','"+q2+"','"+t2+"','"+imp2+"',"+sauve+")";
-        console.log(sql);
+        if(req.param("update")){
+            update = true;
+            var id_cr = req.param("id_cr");
+            sql = "update neocles_compte_rendu set id_manager = "+id+", qual_positif = '"+q1+"', technicite_positif = '"+t1+"', implication_positif = '"+imp1+"', qual_ameliorer = '"+q2+"', technicite_ameliorer = '"+t2+"', implication_ameliorer = '"+imp2+"', valider = "+sauve+" where id = " + id_cr;     
+        }    
         Neocles_manager.query(sql, function (err) {
             if(err) return res.send(err);
             return res.redirect('/coaching/create');
         })
+
     }
 
 };
